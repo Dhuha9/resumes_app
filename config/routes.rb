3289d341478e386c62edce353f5276f9444e8766
Dhuha9 <dhuha9hasan@gmail.com>
2022-07-02 20:49:23 +0300

    feat: add routes for controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :resumes do
+    resource :work_experiences
+    resource :certificates
+    resource :projects
+  end
+  root to: 'resumes#index'
 end
