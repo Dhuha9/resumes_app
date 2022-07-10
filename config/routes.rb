d57c63ff53afcef7026e0f9b7213844636250c99
Dhuha9 <dhuha9hasan@gmail.com>
2022-07-10 18:51:53 +0300

    fix: router resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :resumes do
-    resource :work_experiences
-    resource :certificates
-    resource :projects
+    resources :work_experiences
+    resources :certificates
+    resources :projects
   end
   root to: 'resumes#index'
 end
